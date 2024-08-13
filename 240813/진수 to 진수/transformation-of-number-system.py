0d4d8f03912a28e6c6fab52e32d57e1095473f14
ji252303 <95694431+ji252303@users.noreply.github.com>
2024-08-13 23:20:42 +0900

    Title: 진수 to 진수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240813/진수 to 진수/transformation-of-number-system.py
+++ 240813/진수 to 진수/transformation-of-number-system.py
@@ -2,9 +2,11 @@ a, b = map(int,input().split())
 
 n = str(input())
 
-change = int(n, a)
 
-result = format(change, 'b')  # b진수로 변환하여 문자열로 출력
+if a in [2, 8]:
+    change = int(n, a)
 
-# 결과 출력
-print(result)
+    result = format(change, 'b')  # b진수로 변환하여 문자열로 출력
+
+    # 결과 출력
+    print(result)
